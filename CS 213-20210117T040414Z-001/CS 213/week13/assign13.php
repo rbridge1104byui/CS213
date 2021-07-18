83fbf704f4a708bd82d05659d3a1913f72dfb4da
robert-bridge <robert.bridge@salesforce.com>
2021-07-18 02:07:38 -0400

    final commit

--- CS 213-20210117T040414Z-001/CS 213/week13/assign13.php
+++ CS 213-20210117T040414Z-001/CS 213/week13/assign13.php
@@ -2,41 +2,28 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
-    $datae = array();
-    $datae[] = array(
-        'performance' => $_POST['performance'],
-        'first_name' => $_POST['first_name'],
-        'last_name' => $_POST['last_name'],
-        'student_id' => $_POST['student_id'],
-        'first_name_2' => $_POST['first_name_2'],
-        'first_name_2' => $_POST['first_name_2'],
-        'student_id_2' => $_POST['student_id_2'],
-        'skill' => $_POST['skill'],
-        'instrument' => $_POST['instrument'],
-        'location' => $_POST['location'],
-        'room' => $_POST['room'],
-        'time_slot' => $_POST['time_slot'],
-    );
+    
     //echo json_encode ($datae);
-    $myfile = "/Users/robert.bridge/Documents/School:Church Docs/School Stuff/CS213/CS213/CS 213-20210117T040414Z-001/CS 213/week13/documents/students.txt";
-    $fh = fopen($myfile, 'a') or die ("can't open file");
-    fwrite($fh, json_encode($datae));
+    $myfile = "data/students.txt";
+    $fh = fopen($myfile, 'a+') or die ("can't open file");
+    fwrite($fh, json_encode($_POST));
     fclose($fh);
-
-}
-/*
-if(isset($_GET['request'])){
-    $request = $_GET['request'];
- }
- 
- // Fetch records 
- if($request == 1){
-    $myfile = "/Users/robert.bridge/Documents/School:Church Docs/School Stuff/CS213/CS213/CS 213-20210117T040414Z-001/CS 213/week13/documents/students.txt";
-
-    echo file_get_contents($myfile);
+    //echo file_get_contents($myfile);
+    //return file_get_contents($myfile);
+    header("Location: assign13.html");
     exit;
 
- }*/
+}
+
+//if ($_SERVER['REQUEST_METHOD'] ==='GET'){
+  
+    $myfile = "data/students.txt";
+
+
+    print json_encode( file_get_contents($myfile));
+    exit;
+
+ //}
 
 
 ?>
